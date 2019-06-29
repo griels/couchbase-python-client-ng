aafe0f20c28b0b82691a66cf7807da0da16636ad
Ellis Breen <ellis.breen@couchbase.com>
2019-06-29 13:16:28 +0100

    PYCBC-585: Implement collections support
    Change-Id: I65d3b1d26b4a416ce8657337bb90ab84e315d15c

--- src/lcb_v4_backport.h
+++ src/lcb_v4_backport.h
@@ -74,6 +74,7 @@ typedef lcb_error_t lcb_STATUS;
     (cmd)->create = 1;
 #define lcb_cmdcounter_timeout(cmd, x) (cmd)->exptime = x;
 #define lcb_cmdcounter_expiration(cmd, x) (cmd)->exptime = x
+#define lcb_cmdcounter_collection(...) 0
 #define lcb_cmdstore_flags(CMD, VAL) cmd->flags = VAL;
 /*#define lcb_cmdgetreplica_create_all(ptr) (*(ptr))->strategy = LCB_REPLICA_ALL
 #define lcb_cmdgetreplica_create_select(ptr, INDEX) \
