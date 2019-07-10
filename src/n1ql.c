d7abafe9e449f5ddf0cb65cf701b060534b34eba
Ellis Breen <ellis.breen@couchbase.com>
2019-07-10 18:02:58 +0100

    More analytics tweaks
    Change-Id: Ibc3c5d0c276deeb8c0ef8247b41a9f5721415bcd

--- src/n1ql.c
+++ src/n1ql.c
@@ -224,7 +224,7 @@ lcb_STATUS pycbc_handle_analytics(const pycbc_Bucket *self,
             lcb_cmdanalytics_query(cmd, params, nparams);
             lcb_cmdanalytics_handle(cmd, &vres->base.u.analytics);
             if (host) {
-                pycbc_cmdanalytics_host(cmd, host);
+                //pycbc_cmdanalytics_host(cmd, host);
             }
             PYCBC_TRACECMD_SCOPED_NULL(rc,
                                        analytics,
@@ -289,7 +289,8 @@ TRACED_FUNCTION(LCBTRACE_OP_REQUEST_ENCODING,
                 unsigned nparams,
                 const char *host,
                 int is_prepared,
-                int is_xbucket)
+                int is_xbucket,
+                int is_analytics)
 {
     PyObject *ret = NULL;
     pycbc_MultiResult *mres;
@@ -314,7 +315,7 @@ TRACED_FUNCTION(LCBTRACE_OP_REQUEST_ENCODING,
 
     static pycbc_query_handler handlers[] = {pycbc_handle_n1ql,
                                              pycbc_handle_analytics};
-    rc = (handlers[host ? 1 : 0])(
+    rc = (handlers[is_analytics])(
             self, params, nparams, host, is_prepared, is_xbucket, mres, vres, context);
     if (rc != LCB_SUCCESS) {
         PYCBC_EXC_WRAP(PYCBC_EXC_LCBERR, rc, "Couldn't schedule n1ql query");
@@ -354,7 +355,8 @@ pycbc_Bucket__n1ql_query(pycbc_Bucket *self, PyObject *args, PyObject *kwargs)
                               nparams,
                               NULL,
                               prepared,
-                              cross_bucket);
+                              cross_bucket,
+                              0);
     return result;
 }
 
@@ -380,6 +382,7 @@ pycbc_Bucket__cbas_query(pycbc_Bucket *self, PyObject *args, PyObject *kwargs)
                               nparams,
                               host,
                               0,
-                              0);
+                              0,
+                              1);
     return result;
 }
