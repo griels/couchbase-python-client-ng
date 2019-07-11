ce1a9180742dc2e4735f38b79a27d8776ab6959c
Ellis Breen <ellis.breen@couchbase.com>
2019-07-11 17:30:04 +0100

    Revert diagnostics test tweak
    Change-Id: I6c047cf09fbfa2e8243b227bafe151f2a5892808

--- couchbase/tests_v3/cases/scenarios_t.py
+++ couchbase/tests_v3/cases/scenarios_t.py
@@ -499,12 +499,9 @@ class Scenarios(ConnectionTestCase):
 
     def test_diagnostics(self  # type: Scenarios
                          ):
-        try:
-            diagnostics = self.cluster.diagnostics(timeout=(5 if self.is_mock else None))
-        except couchbase.exceptions.TimeoutError:
-            if self.is_mock:
-                raise SkipTest("LCB Diagnostics still blocks indefinitely with mock: {}".format(traceback.format_exc()))
-
+        if self.is_mock:
+            raise SkipTest("LCB Diagnostics still blocks indefinitely with mock")
+        diagnostics = self.cluster.diagnostics()
         self.assertRegex(diagnostics.sdk(), r'.*PYCBC.*')
         self.assertGreaterEqual(diagnostics.version(), 1)
         self.assertIsNotNone(diagnostics.id())
