fce4dd7ab72ebe8579b595f1a19f9de9f7b29247
Ellis Breen <ellis.breen@couchbase.com>
2019-07-11 17:45:20 +0100

    Preliminary FTS Index implemention for SDK3
    Change-Id: I007bbd27d5f74904cae8bbc05db90cb79ae4ada7

--- couchbase/tests_v3/cases/scenarios_t.py
+++ couchbase/tests_v3/cases/scenarios_t.py
@@ -58,6 +58,7 @@ import couchbase.admin
 import couchbase_core.tests.analytics_harness
 from couchbase_core.cluster import ClassicAuthenticator
 from couchbase_core.connstr import ConnectionString
+from couchbase.admin import IndexType, SourceType
 
 
 class ClusterTestCase(ConnectionTestCase):
@@ -514,6 +515,8 @@ class Scenarios(CollectionTestCase):
     def test_cluster_search(self):
         if self.is_mock:
             raise SkipTest("FTS not supported by mock")
+        self.cluster.search_indexes().upsert('testindex', IndexType.INDEX,
+                                             SourceType.COUCHBASE, "default")
         x = list(self.cluster.search_query("testindex", "testquery"))
 
     def test_diagnostics(self  # type: Scenarios
@@ -523,6 +526,8 @@ class Scenarios(CollectionTestCase):
         except couchbase.exceptions.TimeoutError:
             if self.is_mock:
                 raise SkipTest("LCB Diagnostics still blocks indefinitely with mock: {}".format(traceback.format_exc()))
+            else:
+                raise
 
         self.assertRegex(diagnostics.sdk(), r'.*PYCBC.*')
         self.assertGreaterEqual(diagnostics.version(), 1)
@@ -533,6 +538,10 @@ class Scenarios(CollectionTestCase):
             self.assertIn(type(value.type()), (ServiceType, str))
             self.assertIn(type(value.id()), ANY_STR)
             self.assertIn(type(value.local()), ANY_STR)
+
+    def test_cluster_search(self):
+        x=self.cluster.search_query("testindex","testquery")
+        y=list(x)
 
     def test_multi(self):
         self.coll.upsert_multi({"Fred": "Wilma", "Barney": "Betty"})
