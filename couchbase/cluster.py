e96e995d9131d7e2092c4556b0a983fd44b12c61
Ellis Breen <ellis.breen@couchbase.com>
2019-07-11 19:50:53 +0100

    N1QL Query Options/Metrics
    Change-Id: Ice28c0487c746f02b3a2645c4a88b0bee5ea0816

--- couchbase/cluster.py
+++ couchbase/cluster.py
@@ -137,7 +137,7 @@ class Cluster:
               *options,  # type: QueryOptions
               **kwargs  # type: Any
               ):
-        # type: (...) -> QueryResult
+        # type: (...) -> IQueryResult
         """
         Perform a N1QL query.
 
