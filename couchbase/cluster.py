688100f8bb1eeff94c01f1c51c9d4c5d95e2281b
Ellis Breen <ellis.breen@couchbase.com>
2019-07-11 18:57:19 +0100

    Analytics
    Change-Id: Ibc3c5d0c276deeb8c0ef8247b41a9f5721415bcd

--- couchbase/cluster.py
+++ couchbase/cluster.py
@@ -41,6 +41,10 @@ def options_to_func(orig,  # type: U
     return invocation(orig)
 
 
+class AnalyticsOptions(OptionBlock):
+    pass
+
+
 class Cluster:
     clusterbucket = None  # type: CoreBucket
 
@@ -137,7 +141,7 @@ class Cluster:
         except Exception as e:
             raise failtype(str(e))
 
-    def analytics_query(self,
+    def analytics_query(self,  # type: Cluster
                         statement,  # type: str,
                         *options,  # type: AnalyticsOptions
                         **kwargs
@@ -151,6 +155,7 @@ class Cluster:
         Throws Any exceptions raised by the underlying platform - HTTP_TIMEOUT for example.
         :except ServiceNotFoundException - service does not exist or cannot be located.
         """
+
         return AnalyticsResult(self._operate_on_cluster(CoreBucket.analytics_query, AnalyticsException, statement, **forward_args(kwargs,*options)))
 
     def search_query(self,
