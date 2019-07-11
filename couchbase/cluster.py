707b59870731e02480b627bb0b0c30b72eff997f
Ellis Breen <ellis.breen@couchbase.com>
2019-07-11 22:39:46 +0100

    PYCBC-571: Move QueryOptions into N1QL module.
    Change-Id: Ica9c914d354745e7310b179223551209bdbc959c

--- couchbase/cluster.py
+++ couchbase/cluster.py
@@ -4,7 +4,7 @@ from couchbase.diagnostics import DiagnosticsResult, EndPointDiagnostics, IDiagn
 from couchbase.fulltext import ISearchResult, SearchResult, SearchOptions
 from couchbase_core.fulltext import Query
 from .analytics import AnalyticsResult
-from .n1ql import QueryResult, IQueryResult
+from .n1ql import QueryResult, IQueryResult, QueryOptions
 from .options import OptionBlock, forward_args, OptionBlockDeriv
 from .bucket import BucketOptions, Bucket, CoreBucket
 from couchbase_core.cluster import Cluster as SDK2Cluster, Authenticator as SDK2Authenticator
@@ -20,9 +20,6 @@ import couchbase.admin
 
 T = TypeVar('T')
 
-
-class QueryMetrics(object):
-    pass
 
 CallableOnOptionBlock = Callable[[OptionBlockDeriv, Any], Any]
 
@@ -52,26 +49,6 @@ def options_to_func(orig,  # type: U
 
 class AnalyticsOptions(OptionBlock):
     pass
-
-
-class QueryOptions(OptionBlock, IQueryResult):
-    @property
-    @abstractmethod
-    def is_live(self):
-        return False
-
-    def __init__(self, statement=None, parameters=None, timeout=None):
-
-        """
-        Executes a N1QL query against the remote cluster returning a IQueryResult with the results of the query.
-        :param statement: N1QL query
-        :param options: the optional parameters that the Query service takes. See The N1QL Query API for details or a SDK 2.0 implementation for detail.
-        :return: An IQueryResult object with the results of the query or error message if the query failed on the server.
-        :except Any exceptions raised by the underlying platform - HTTP_TIMEOUT for example.
-        :except ServiceNotFoundException - service does not exist or cannot be located.
-
-        """
-        super(QueryOptions, self).__init__(statement=statement, parameters=parameters, timeout=timeout)
 
 
 ClusterManager = Admin
