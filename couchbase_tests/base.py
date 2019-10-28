3cf3a9d7a813c19c8dac717b480750aaa41d169a
Ellis Breen <ellis.breen@couchbase.com>
2019-10-28 17:06:37 +0000

    PYCBC-646: Add View Management
    Change-Id: I2e8c5af21041e6e47da358c20b9d66cf7131e7e9
    (cherry picked from commit d167764f461eae0d86f66402fea76469324e4e2d)

--- couchbase_tests/base.py
+++ couchbase_tests/base.py
@@ -528,7 +528,7 @@ class CouchbaseTestCase(ResourcedTestCase):
 
 class ConnectionTestCaseBase(CouchbaseTestCase):
     def __init__(self, *args, **kwargs):
-        self.cb =None # type: self.factory
+        self.cb =None
         super(ConnectionTestCaseBase,self).__init__(*args,**kwargs)
     def checkCbRefcount(self):
         if not self.should_check_refcount:
@@ -834,7 +834,7 @@ class CollectionTestCase(ClusterTestCase):
             pass
 
 
-class DDocTestCase(ConnectionTestCase):
+class DDocTestCase(RealServerTestCase):
     pass
 
 
