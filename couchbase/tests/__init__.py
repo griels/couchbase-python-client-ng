c9587eb29cf1e0040a4eb78ee760d0e9dc5b4879
Ellis Breen <ellis.breen@couchbase.com>
2020-02-27 16:12:33 +0000

    PYCBC-736: gcouchbase support
    Motivation
    ----------
    The existing gevent support only works for gevent<1.3,
    using methods that are no longer present in later gevent
    versions. Unfortunately this means that gcouchbase doesn't
    work with Python 3, as gevent 1.3 is the first version
    that works with it. We should support later gevents.
    
    Changes
    -------
    Create a new timer each time callback is called instead of
    using unsupported behaviour to do so.
    
    Results
    -------
    Tests pass
    
    Change-Id: Id6e185a43a5354ec8bdb64c88de37b66949add4c

--- couchbase/tests/__init__.py
+++ couchbase/tests/__init__.py
@@ -7,6 +7,7 @@ caseslist += [couchbase_v2.tests.cases]
 import couchbase.tests_v3.cases as latest_cases
 import acouchbase as acouchbase_tests
 import txcouchbase.tests as txcouchbase_tests
+import gcouchbase.tests as gcouchbase_test
 
-caseslist += [latest_cases, acouchbase_tests, txcouchbase_tests]
+caseslist += [latest_cases, acouchbase_tests, txcouchbase_tests, gcouchbase_test]
 import couchbase_tests.test_sync as test_sync
