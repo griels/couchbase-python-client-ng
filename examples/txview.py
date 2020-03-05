1d74c9dd918dbebf04de86c3fdd46ba21e2c77e1
Ellis Breen <ellis.breen@couchbase.com>
2020-03-05 10:35:13 +0000

    PYCBC-816: Add Python-level SDK3 Twisted implementation
    Motivation
    ----------
    Now we have the core Twisted functionality working with the
    SDK3 codebase, we can add Twisted support for couchbase Collections.
    
    Changes
    -------
    Add auto-failing timeout to Twisted tests.
    SDK3-style results now returned by Twisted.
    Changed the way txcouchbase sets callbacks on the returned result -
    set_callbacks must be used rather than direct member access.
    
    Results
    -------
    All tests passing except some multi-operations in Twisted mode -
    this is not part of the current SDK3 spec and is to be defined,
    therefore we skip these tests for now.
    
    Change-Id: I618d7a777b8a0a528ae8863c28b0133ade472f69

--- examples/txview.py
+++ examples/txview.py
@@ -7,6 +7,6 @@ def on_view_rows(res):
         print "Got row", row.key
 
 cb = Bucket('couchbase://localhost/beer-sample')
-d = cb.queryAll("beer", "brewery_beers", limit=20)
+d = cb.view_query("beer", "brewery_beers", limit=20)
 d.addCallback(on_view_rows)
 reactor.run()
