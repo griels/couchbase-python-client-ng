1d3cdeb813c9a28a455eb8a1c8edf16958404642
Ellis Breen <ellis.breen@couchbase.com>
2020-03-11 19:42:00 +0000

    Just set and err tx failing And test_list/map/queue
    Change-Id: I8bb2ae655ddd3c18cc5ab0deb044f82ac056cf9e

--- txcouchbase/tests/test_ops.py
+++ txcouchbase/tests/test_ops.py
@@ -40,7 +40,6 @@ class OperationTestCase(Base):
         return d
 
     def test_simple_get(self):
-        raise SkipTest()
         cb = self.make_connection()
         key = self.gen_key("test_simple_get")
         value = "simple_value"
