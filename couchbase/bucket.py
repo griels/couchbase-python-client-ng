bd00e4f5119f926a4ed97a39fe256408f25eb676
Ellis Breen <ellis.breen@couchbase.com>
2020-03-12 14:27:30 +0000

    PYCBC-815, PYCBC-816:  Views working now
    Works on CC
    On OP: Just fails test_list/map/queue/multi_errors
    
    Change-Id: Ie07f9dfd09aa1ae96a93fd7165b102acd0e1c367

--- couchbase/bucket.py
+++ couchbase/bucket.py
@@ -257,8 +257,10 @@ class Bucket(CoreClientDatastructureWrap):
         :param ViewOptions view_options: Options to use when querying a view index.
         :return: ViewResult containing the view results
         """
-        res = CoreClient.view_query(self, design_doc, view_name, **forward_args(kwargs, *view_options))
-        return res# ViewResult(res)
+        final_kwargs={'itercls':ViewResult}
+        final_kwargs.update(kwargs)
+        res = CoreClient.view_query(self, design_doc, view_name, **forward_args(final_kwargs, *view_options))
+        return res
 
     def view_indexes(self  # type: Bucket
                      ):
