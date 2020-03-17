cb320f1601a1511cf4d575afed2a0935916481a1
Ellis Breen <ellis.breen@couchbase.com>
2020-03-17 20:21:03 +0000

    Still working
    Change-Id: I349358173f9e1562b24e35df3bbcbd5fcf90eb5c

--- couchbase/cluster.py
+++ couchbase/cluster.py
@@ -244,15 +244,17 @@ class Cluster(CoreClient):
             return bucket_factory(connection_string=connstr, name=bname, admin=self._admin, **kwargs)
         cluster_opts.update(
             bucket_factory=corecluster_bucket_factory)
-
-        self.__cluster=None
-        super(Cluster,self).__init__(connection_string=str(self.connstr), _flags=_flags, _iops=_iops,_conntype=_LCB.LCB_TYPE_CLUSTER)#, **self._clusteropts)
-        #self._clusterclient = None#super(Cluster,self)
-
-    @property
-    def _cluster(self):
-        self.__cluster = CoreCluster(connection_string=self.connstr, **self._clusteropts)  # type: CoreCluster
+        self._cluster = CoreCluster(connection_string, **cluster_opts)  # type: CoreCluster
         self._authenticate(self._authenticator)
+        self._cluster =None
+        super(Cluster,self).__init__(connection_string=str(self.connstr), _flags=_flags, _iops=_iops,_conntype=_LCB.LCB_TYPE_CLUSTER, **self._clusteropts)
+        self._clusterclient = None#super(Cluster,self)
+
+    def _do_ctor_connect(self, *args, **kwargs):
+        """This should be overidden by subclasses which want to use a
+        different sort of connection behavior
+        """
+        self._connect()
 
     @staticmethod
     def connect(connection_string,  # type: str
@@ -281,11 +283,11 @@ class Cluster(CoreClient):
         credentials = authenticator.get_credentials()
         self._clusteropts = credentials.get('options', {})
         self._clusteropts['bucket'] = "default"
-        #auth = credentials.get('options')
-        #self._admin = Admin(auth.get('username'), auth.get('password'), connstr=str(self.connstr))
+        auth = credentials.get('options')
+
     @property
     def _admin(self):
-        return self
+        return self#self._admin = Admin(auth.get('username'), auth.get('password'), connstr=str(self.connstr))
 
     # TODO: There should be no reason for these kwargs.  However, our tests against the mock
     # will all fail with auth errors without it...  So keeping it just for now, but lets fix it
