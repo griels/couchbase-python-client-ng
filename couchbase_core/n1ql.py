4b2ecad05053c62ff9a89b88c96346d160b3fa86
Ellis Breen <ellis.breen@couchbase.com>
2020-03-26 12:37:47 +0000

    Closing in on AnalyticsQuery._submit_request (TxCluster._cbas_query) not producing a deferred
    Change-Id: I54f4efb1e73bc194e716eb4349fd441ef09e28b3

--- couchbase_core/n1ql.py
+++ couchbase_core/n1ql.py
@@ -413,9 +413,11 @@ class N1QLRequest(object):
         self.meta_lookahead = meta_lookahead
 
     def _submit_query(self):
-        return self._parent._n1ql_query(self._params.encoded,
+        result= self._parent._n1ql_query(self._params.encoded,
                                         not self._params.adhoc,
                                         cross_bucket=self._params.cross_bucket)
+        import traceback
+        print("Got result {} on {} at {}".format(result,    self._parent,     traceback.format_stack()))
 
     def _start(self):
         if self._mres:
--- txcouchbase/tests/test_txcluster.py
+++ txcouchbase/tests/test_txcluster.py
@@ -16,19 +16,16 @@
 from couchbase.exceptions import UnknownHostError
 from twisted.internet import defer
 
-from couchbase.management.analytics import CreateDatasetOptions
 from couchbase_core.exceptions import (
-    BucketNotFoundError,
     ObjectDestroyedError)
 
 from couchbase_tests.base import ConnectionTestCase
 from couchbase_core.connstr import ConnectionString
 from txcouchbase.tests.base import gen_base
-from txcouchbase.bucket import TxBucket, TxCluster, BatchedAnalyticsResult
+from txcouchbase.bucket import TxBucket
 from nose.tools import timed
 import sys
 from unittest import SkipTest
-from couchbase.cluster import Cluster as SyncCluster
 
 Base = gen_base(ConnectionTestCase)
 
@@ -95,143 +92,3 @@ class BasicClusterTest(Base):
 
 
 
-import couchbase.tests_v3.cases.cluster_t
-import couchbase.tests_v3.cases.analytics_t
-from couchbase_v2.bucket import Bucket as V2Bucket
-
-import asyncio
-
-class RewrappedCluster(TxCluster):
-    def __init__(self, *args, **kwargs):
-        async def wrapper():
-            return super(RewrappedCluster, self).__init__(*args,**kwargs)
-        self.evloop = asyncio.get_event_loop()
-        self.evloop.run_until_complete(wrapper())
-    async def wrapper(self, meth, *args, **kwargs):
-        result=meth(super(RewrappedCluster,self), *args, **kwargs)
-        return await result
-    def _wrap(self,  # type: TxDeferredClient
-              meth, *args, **kwargs):
-        """
-        Wraps a Twisted Cluster back into a synchronous cluster,
-        for testing purposes
-        """
-        #if not self.connected:
-        #    return self._connectSchedule(self._wrap, meth, *args, **kwargs)
-
-
-        result=self.evloop.run_until_complete(self.wrapper(meth, *args, **kwargs))
-        return result
-
-    ### Generate the methods
-    def _meth_factory(meth, name):
-        def ret(self, *args, **kwargs):
-            return self._wrap(meth, *args, **kwargs)
-        return ret
-
-    locals().update(TxCluster._gen_memd_wrappers(_meth_factory))
-    for x in TxCluster._MEMCACHED_OPERATIONS:
-        if locals().get(x+'_multi', None):
-            locals().update({x+"Multi": locals()[x+"_multi"]})
-
-import os
-if False or os.getenv("PYCBC_TXCLUSTER_TESTS"):
-    class TxClusterTests(couchbase.tests_v3.cases.analytics_t.AnalyticsTestCase):
-        #@property
-        #def factory(self):
-        #    return self._factory
-        @property
-        def cluster_factory(self):
-            return RewrappedCluster
-        def realSetUp(self):
-            #self._factory=V2Bucket
-            super(couchbase.tests_v3.cases.analytics_t.AnalyticsTestCase, self).setUp()
-            #self._factory=Base.gen_cluster
-            #self.cluster=self.make_connection()
-from twisted.internet.task import deferLater
-from twisted.internet import reactor
-from twisted.internet.defer import inlineCallbacks
-if True:
-    class TxAnalyticsTest(gen_base(couchbase.tests_v3.cases.analytics_t.AnalyticsTestCase)):
-        def setUp(self):
-            self._factory=SyncCluster
-            super(TxAnalyticsTest, self).setUp()
-            # if self.is_mock:
-            #     raise SkipTest("analytics not mocked")
-            # if int(self.get_cluster_version().split('.')[0]) < 6:
-            #     raise SkipTest("no analytics in {}".format(self.get_cluster_version()))
-            # self.mgr = self.cluster.analytics_indexes()
-            # self.dataset_name = 'test_beer_dataset'
-            # # create a dataset to query
-            # self.mgr.create_dataset(self.dataset_name, 'beer-sample', CreateDatasetOptions(ignore_if_exists=True))
-            # def has_dataset(name):
-            #     datasets = self.mgr.get_all_datasets()
-            #     return [d for d in datasets if d.dataset_name == name][0]
-            # self.try_n_times(10, 3, has_dataset, self.dataset_name)
-            # # connect it...
-            #
-            # self.mgr.connect_link()
-            self._factory=self.gen_cluster
-            self.cluster=self.make_connection()
-
-
-        def sleep(self, secs):
-            return deferLater(reactor, secs, lambda: None)
-
-        def try_n_times(self, num_times, seconds_between, func, *args, **kwargs):
-            if not isinstance(self.cluster, TxCluster):
-                return super(TxAnalyticsTest, self).try_n_times(num_times, seconds_between, func, *args, **kwargs)
-            class ResultHandler(object):
-                def __init__(self, parent):
-                    self.remaining=num_times
-                    self._parent=parent
-                def start(self):
-                    ret = func(*args, **kwargs)
-                    result = ret.addCallback(self.success)
-                    ret.addErrback(self.on_fail)
-                    return result
-                def success(self, result):
-                    return result
-                def on_fail(self, deferred_exception):
-                    deferred_exception.catch(Exception)
-                    return None
-                    #if self.remaining:
-                    #    self.remaining-=1
-                    #    self.sleep(seconds_between).addCallback()
-                    #else:
-                    #self._parent.fail("unsuccessful {} after {} times, waiting {} seconds between calls".format(func, num_times, seconds_between))
-            return ResultHandler(self).start()
-
-        def checkResult(self, result, callback):
-            return result#result.addCallback(callback)
-
-        def _verify(self, d  # type: Base
-                        ):
-            import logging
-            def verify(o):
-                logging.error("in callback with {}".format(o))
-                self.assertIsInstance(o, BatchedAnalyticsResult)
-                rows = [r for r in o]
-                logging.error("End of callback")
-
-            result= d.addCallback(verify)
-            d.addErrback(self.mock_fallback)
-            logging.error("ready to return")
-            return result
-
-        def assertQueryReturnsRows(self, query, *options, **kwargs):
-            d = self.cluster.analytics_query(query, *options, **kwargs)
-
-            def query_callback(result):
-                self.assertIsInstance(result, BatchedAnalyticsResult)
-
-                rows = result.rows()
-                if len(rows) > 0:
-                    return rows
-                raise Exception("no rows in result")
-            return d.addCallback(query_callback)
-
-        locals().update()
-        @property
-        def factory(self):
-            return self._factory
