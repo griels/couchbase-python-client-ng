0507383f82589692d367d5707001aef7cee1077f
Ellis Breen <ellis.breen@couchbase.com>
2020-04-08 12:24:12 +0100

    Moved common setup stuff to base
    Change-Id: I5c5e4e6ca6b4237d69463b552c0d1ebed365ec89

--- couchbase_tests/base.py
+++ couchbase_tests/base.py
@@ -19,6 +19,10 @@ import sys
 import types
 import platform
 import warnings
+from abc import abstractmethod
+
+from couchbase.cluster import ClassicAuthenticator, PasswordAuthenticator
+from couchbase_core.connstr import ConnectionString
 
 from couchbase.management.analytics import CreateDatasetOptions
 from collections import defaultdict
@@ -999,3 +1003,49 @@ class AnalyticsTestCaseBase(CollectionTestCase):
             return self.mgr.connect_link()
 
         self.try_n_times(10, 3, has_dataset, self.dataset_name, on_success=on_dataset)
+
+
+class AsyncClusterTestCase(object):
+    def _instantiate_txcluster(self, connstr_nobucket, **kwargs):
+        # it seems the mock requires ClassicAuthenticator to work (hence its use in the ClusterTestCase)
+        # TODO: resolve this
+
+        auth_type = ClassicAuthenticator if self.is_mock else PasswordAuthenticator
+        mock_hack = {'bucket':self.cluster_info.bucket_name} if self.is_mock else {}
+        return self.cluster_class(connection_string=str(connstr_nobucket),
+                                    authenticator=auth_type(self.cluster_info.admin_username,
+                                                 self.cluster_info.admin_password), **mock_hack)
+
+    def _get_connstr_and_bucket_name(self,
+                                     args,  # type: List[Any]
+                                     kwargs):
+        connstr = args.pop(0) if args else kwargs.pop('connection_string')
+        connstr_nobucket = ConnectionString.parse(connstr)
+        bucket=connstr_nobucket.bucket
+        connstr_nobucket.bucket = None
+        return connstr_nobucket, bucket
+
+    def gen_cluster(self,  # type: AsyncClusterTestCase
+                    *args,
+                    **kwargs):
+        # type: (...) -> Cluster
+        args = list(args)
+        connstr_nobucket, bucket = self._get_connstr_and_bucket_name(args, kwargs)
+        return self._instantiate_txcluster(connstr_nobucket, **kwargs)
+
+    def gen_bucket(self, *args, override_bucket=None, **kwargs):
+        args = list(args)
+        connstr_nobucket, bucket = self._get_connstr_and_bucket_name(args, kwargs)
+        bucket = override_bucket or bucket
+        return self._instantiate_txcluster(connstr_nobucket, **kwargs).bucket(bucket)
+
+    def gen_collection(self,
+                       *args, **kwargs):
+        bucket_result = self.gen_bucket(*args, **kwargs)
+        return bucket_result.default_collection()
+
+    @property
+    @abstractmethod
+    def cluster_class(self):
+        # type: (...) -> Cluster
+        pass
