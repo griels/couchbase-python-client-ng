aa7bbe59ec3215bd2b23ad4deee48e1c5408b68e
Ellis Breen <ellis.breen@couchbase.com>
2020-04-10 13:50:30 +0100

    PYCBC-796: All the v2 tests need to be converted to v3
    Fixed ConnectionTest
    
    Change-Id: Iadb2bc5eda984581ed24febbba1f49a0a13b6927

--- couchbase/tests_v3/cases/ported/arithmetic_t.py
+++ couchbase/tests_v3/cases/ported/arithmetic_t.py
@@ -15,41 +15,43 @@
 # limitations under the License.
 #
 
-from couchbase_v2.exceptions import (DocumentNotFoundException, DeltaBadvalException)
-from couchbase_tests.base import ConnectionTestCase
+from couchbase.exceptions import (DocumentNotFoundException, DeltaBadvalException)
+from couchbase_tests.base import CollectionTestCase
+from couchbase.collection import SignedInt64, DeltaValue
 
-
-class ArithmeticTest(ConnectionTestCase):
-
+class ArithmeticTest(CollectionTestCase):
+    def setUp(self):
+        super(ArithmeticTest, self).setUp()
+        self.bin_coll=self.cb.binary()
     def test_trivial_incrdecr(self):
         key = self.gen_key("trivial_incrdecr")
         self.cb.remove(key, quiet=True)
-        rv_arith = self.cb.counter(key, initial=1, delta=1)
+        rv_arith = self.bin_coll.increment(key, initial=SignedInt64(1), delta=DeltaValue(1))
         rv_get = self.cb.get(key)
 
-        self.assertEqual(rv_arith.value, 1)
-        self.assertEqual(int(rv_get.value), 1)
+        # self.assertEqual(rv_arith.content, 1) SDK3 MutationResult has no content
+        self.assertEqual(int(rv_get.content), 1)
 
-        rv = self.cb.counter(key)
-        self.assertEqual(rv.value, 2)
+        rv = self.bin_coll.increment(key)
+        # self.assertEqual(rv.content, 2)
 
-        rv = self.cb.counter(key, delta=-1)
-        self.assertEqual(rv.value, 1)
-        self.assertEqual(int(self.cb.get(key).value), 1)
+        rv = self.bin_coll.decrement(key, delta=DeltaValue(1))
+        # self.assertEqual(rv.content, 1)
+        self.assertEqual(int(self.cb.get(key).content), 1)
 
-        rv = self.cb.counter(key, delta=-1)
-        self.assertEqual(rv.value, 0)
-        self.assertEqual(int(self.cb.get(key).value), 0)
+        rv = self.bin_coll.decrement(key, delta=DeltaValue(1))
+        # self.assertEqual(rv.content, 0)
+        self.assertEqual(int(self.cb.get(key).content), 0)
 
     def test_incr_notfound(self):
         key = self.gen_key("incr_notfound")
         self.cb.remove(key, quiet=True)
-        self.assertRaises(DocumentNotFoundException, self.cb.counter, key)
+        self.assertRaises(DocumentNotFoundException, self.bin_coll.increment, key)
 
     def test_incr_badval(self):
         key = self.gen_key("incr_badval")
         self.cb.upsert(key, "THIS IS SPARTA")
-        self.assertRaises(DeltaBadvalException, self.cb.counter, key)
+        self.assertRaises(DeltaBadvalException, self.bin_coll.increment, key)
 
     def test_incr_multi(self):
         keys = self.gen_key_list(amount=5, prefix="incr_multi")
@@ -57,27 +59,27 @@ class ArithmeticTest(ConnectionTestCase):
         def _multi_lim_assert(expected):
             for k, v in self.cb.get_multi(keys).items():
                 self.assertTrue(k in keys)
-                self.assertEqual(v.value, expected)
+                self.assertEqual(v.content, expected)
 
         self.cb.remove_multi(keys, quiet=True)
-        self.cb.counter_multi(keys, initial=5)
+        self.bin_coll.increment_multi(keys, initial=SignedInt64(5))
         _multi_lim_assert(5)
 
-        self.cb.counter_multi(keys)
+        self.bin_coll.increment_multi(keys)
         _multi_lim_assert(6)
 
-        self.cb.counter_multi(keys, delta=-1)
+        self.bin_coll.decrement_multi(keys, delta=DeltaValue(1))
         _multi_lim_assert(5)
 
-        self.cb.counter_multi(keys, delta=10)
+        self.bin_coll.increment_multi(keys, delta=DeltaValue(10))
         _multi_lim_assert(15)
 
-        self.cb.counter_multi(keys, delta=-6)
+        self.bin_coll.decrement_multi(keys, delta=DeltaValue(6))
         _multi_lim_assert(9)
 
         self.cb.remove(keys[0])
 
-        self.assertRaises(DocumentNotFoundException, self.cb.counter_multi, keys)
+        self.assertRaises(DocumentNotFoundException, self.bin_coll.increment_multi, keys)
 
     def test_incr_extended(self):
         key = self.gen_key("incr_extended")
