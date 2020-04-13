b62ebe4afcde627a4066f40940e5524a6f7c54a7
Ellis Breen <ellis.breen@couchbase.com>
2020-04-13 13:29:45 +0100

    PYCBC-870: fix MacOS build
    Use proper cluster ops with 6.5 DP as well.
    Make TX tests more stable
    
    Try to run search on mock as far as possible, raise SkipTest if
    not supported
    Add logging to deferred verb
    
    Fix up spewer to log to logger
    
    Change-Id: I5de594e36eeb3e3efb4d1f43054b2f8a137cb48b

--- txcouchbase/tests/base.py
+++ txcouchbase/tests/base.py
@@ -15,6 +15,7 @@
 #
 import os
 import sys
+from types import CodeType, FrameType
 from typing import *
 
 import twisted.internet.base
@@ -30,10 +31,67 @@ T = TypeVar('T', bound=ClusterTestCase)
 Factory = Callable[[Any], Client]
 twisted.internet.base.DelayedCall.debug = True
 
+import logging
+import re
+import inspect
 
-if os.getenv("PYCBC_DEBUG_SPEWER"):
-    # enable very detailed call logging
-    sys.settrace(twisted.python.util.spewer)
+
+candidate_pattern=re.compile(r'.*(twisted|tx|couchbase).*')
+
+
+def validate_frame(frame,   # type: FrameType
+                   s):
+    module = get_module_or_filename(frame)
+    return (not module) or candidate_pattern.match(module)
+
+
+def get_module_or_filename(frame):
+    return inspect.getmodule(frame.f_code) or frame.f_code.co_filename
+
+
+def magicmethod(clazz, method):
+    if method not in clazz.__dict__:  # Not defined in clazz : inherited
+        return 'inherited'
+    elif hasattr(super(clazz), method):  # Present in parent : overloaded
+        return 'overloaded'
+    else:  # Not present in parent : newly defined
+        return 'newly defined'
+
+
+def logged_spewer(frame,  # type: FrameType
+                  s,
+                  ignored):
+    """
+    A trace function for sys.settrace that prints every function or method call.
+    """
+    from twisted.python import reflect
+    try:
+        code = frame.f_code
+        co_name = code.co_name
+        co_filename = code.co_filename
+        module_or_filename=get_module_or_filename(frame)
+        if not validate_frame(frame, s):
+            return
+        if 'self' in frame.f_locals:
+            se = frame.f_locals['self']
+            if hasattr(se, '__class__'):
+                clazz = se.__class__
+            else:
+                clazz = type(se)
+            k = reflect.qual(clazz)
+            if magicmethod(clazz,co_name) in ('inherited',):
+                return
+            logging.info('method %s.%s at %s' % (
+                k, co_name, id(se)))
+        else:
+            import inspect
+            logging.info('function %s in module %s at %s, line %s' % (
+                co_name,
+                module_or_filename,
+                co_filename,
+                frame.f_lineno))
+    except:
+        pass
 
 
 def gen_base(basecls,  # type: Type[T]
@@ -55,6 +113,7 @@ def gen_base(basecls,  # type: Type[T]
                 obj.registerDeferred('_dtor', d)
             except Exception as e:
                 raise
+
             def cleanup(*args, **kwargs):
                 return d, None
             self.addCleanup(cleanup)
@@ -76,15 +135,22 @@ def gen_base(basecls,  # type: Type[T]
             return factory or self.gen_collection
 
         def setUp(self):
+            self.setUpTrace(self)
             super(_TxTestCase, self).setUp()
             self.cb = None
 
         def tearDown(self):
-            super(_TxTestCase, self).tearDown()
+            try:
+                super(_TxTestCase, self).tearDown()
+            except Exception as e:
+                raise
+            finally:
+                self.tearDownTrace(self)
 
         @classmethod
         def setUpClass(cls) -> None:
             import inspect
+            cls.setUpTrace(cls)
             if timeout:
                 for name, method in inspect.getmembers(cls,inspect.isfunction):
                     try:
@@ -92,5 +158,23 @@ def gen_base(basecls,  # type: Type[T]
                         getattr(cls,name).timeout=timeout
                     except Exception as e:
                         print(e)
+
+        @staticmethod
+        def setUpTrace(subject):
+            if os.getenv("PYCBC_DEBUG_SPEWER", "").upper() == "TRUE":
+                # enable very detailed call logging
+                subject._oldtrace = sys.gettrace()
+                sys.settrace(logged_spewer)
+
+        @staticmethod
+        def tearDownTrace(subject):
+            oldtrace = getattr(subject, '_oldtrace', None)
+            if oldtrace:
+                sys.settrace(oldtrace)
+
+        @classmethod
+        def tearDownClass(cls):
+            super(_TxTestCase, cls).tearDownClass()
+            cls.tearDownTrace(cls)
 
     return _TxTestCase
