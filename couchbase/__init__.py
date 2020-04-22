a6528de22de9473572228d787e1eaabe1ebfd787
Ellis Breen <ellis.breen@couchbase.com>
2020-04-22 16:29:57 +0100

    Current summary of params
    Change-Id: I8b64878defd95bb6be0c4663937117a3306a1214

--- couchbase/__init__.py
+++ couchbase/__init__.py
@@ -1,2 +1,8 @@
 import couchbase_core._bootstrap
 couchbase_core._bootstrap.do_init()
+
+{'get_multi_result': {'format': 67108868, 'initial': 5, 'quiet': True, 'delta': -6}, 'get_multi': {}
+
+    , 'upsert': {'cas': 8,
+                 'format': 50331650}, 'get': {}, 'get_all_replicas': {}, 'get_and_lock': {}, 'get_and_touch': {},
+ 'get_any_replica': {}}
