3feb4adaf93bc995958590f95dc859bad9e21473
Ellis Breen <ellis.breen@couchbase.com>
2020-04-23 18:44:21 +0100

    Removing factory need for acouchbase
    Change-Id: I0bef297981c7bc0b24722b065184997b234e7e22

--- acouchbase/cluster.py
+++ acouchbase/cluster.py
@@ -18,29 +18,35 @@ from couchbase.cluster import AsyncCluster as V3AsyncCluster
 T = TypeVar('T', bound=CoreClient)
 
 
-def _meth_factory(meth, name):
-    def ret(self, *args, **kwargs):
-        rv = meth(self, *args, **kwargs)
-        ft = asyncio.Future()
-
-        def on_ok(res):
-            ft.set_result(res)
-            rv.clear_callbacks()
-
-        def on_err(res, excls, excval, exctb):
-            err = excls(excval)
-            ft.set_exception(err)
-            rv.clear_callbacks()
-
-        rv.set_callbacks(on_ok, on_err)
-        return ft
-
-    return ret
-
-
 class AIOClientMixin(object):
-    def __new__(cls, *args, **kwargs):
+    def __new__(cls, *args,**kwargs):
+        # type: (...) -> Type[T]
+        if not hasattr(cls, "AIO_wrapped"):
+            for k, method in cls._gen_memd_wrappers(AIOClientMixin._meth_factory).items():
+                setattr(cls, k, method)
+            cls.AIO_wrapped = True
         return super(AIOClientMixin, cls).__new__(cls, *args, **kwargs)
+
+    @staticmethod
+    def _meth_factory(meth, _):
+        def ret(self, *args, **kwargs):
+            rv = meth(self, *args, **kwargs)
+            ft = asyncio.Future()
+
+            def on_ok(res):
+                ft.set_result(res)
+                rv.clear_callbacks()
+
+            def on_err(_, excls, excval, __):
+                err = excls(excval)
+                ft.set_exception(err)
+                rv.clear_callbacks()
+
+            rv.set_callbacks(on_ok, on_err)
+            return ft
+
+        return ret
+
     def __init__(self, connstr=None, *args, **kwargs):
         loop = asyncio.get_event_loop()
         if connstr and 'connstr' not in kwargs:
@@ -100,18 +106,7 @@ class AIOClientMixin(object):
             return self._cft
 
 
-class AsyncBucketFactory(type):
-    @staticmethod
-    def gen_async_bucket(asyncbase  # type: Type[T]
-                         ):
-        # type: (...) -> Type[T]
-        class AIOClientSpecific(AIOClientMixin, asyncbase):
-
-            locals().update(asyncbase._gen_memd_wrappers(_meth_factory))
-        return AIOClientSpecific
-
-
-class AsyncCBCollection(AsyncBucketFactory.gen_async_bucket(BaseAsyncCBCollection)):
+class AsyncCBCollection(AIOClientMixin, BaseAsyncCBCollection):
     def __init__(self,
                  *args,
                  **kwargs
@@ -122,7 +117,7 @@ class AsyncCBCollection(AsyncBucketFactory.gen_async_bucket(BaseAsyncCBCollectio
 Collection = AsyncCBCollection
 
 
-class ABucket(AsyncBucketFactory.gen_async_bucket(V3AsyncBucket)):
+class ABucket(AIOClientMixin, V3AsyncBucket):
     def __init__(self, *args, **kwargs):
         super(ABucket,self).__init__(collection_factory=AsyncCBCollection, *args, **kwargs)
 
@@ -130,7 +125,7 @@ class ABucket(AsyncBucketFactory.gen_async_bucket(V3AsyncBucket)):
 Bucket = ABucket
 
 
-class ACluster(AsyncBucketFactory.gen_async_bucket(V3AsyncCluster)):
+class ACluster(AIOClientMixin, V3AsyncCluster):
     def __init__(self, connection_string, *options, **kwargs):
         super(ACluster, self).__init__(connection_string=connection_string, *options, bucket_factory=Bucket, **kwargs)
 
--- couchbase/cluster.py
+++ couchbase/cluster.py
@@ -823,7 +823,7 @@ class Cluster(CoreClient):
         return mode & _LCB.LCB_SSL_ENABLED != 0
 
 
-class AsyncCluster(AsyncClientMixin,Cluster):
+class AsyncCluster(AsyncClientMixin, Cluster):
     @classmethod
     def connect(cls, connection_string=None, *args, **kwargs):
         return cls(connection_string=connection_string, *args, **kwargs)
