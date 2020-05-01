462219d6d8e4004fbc9cbc9f447fce6f9c3c6ace
Ellis Breen <ellis.breen@couchbase.com>
2020-05-01 11:23:58 +0100

    Revert V2/param scanning stuff
    Change-Id: I980ae6e8a3e3878fec08e3bbe7aab99731c38f47

--- couchbase/tests/__init__.py
+++ couchbase/tests/__init__.py
@@ -3,7 +3,6 @@ from couchbase_tests import caseslist
 import couchbase.tests_v3.cases as latest_cases
 import acouchbase as acouchbase_tests
 import txcouchbase.tests as txcouchbase_tests
-import couchbase_v2.tests as v2_tests
 
-caseslist += [latest_cases, acouchbase_tests, txcouchbase_tests, v2_tests]
+caseslist += [latest_cases, acouchbase_tests, txcouchbase_tests]
 import couchbase_tests.test_sync as test_sync
