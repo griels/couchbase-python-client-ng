de03e12615781d77464f4f3cd90eb81a02ad7920
Ellis Breen <ellis.breen@couchbase.com>
2020-05-13 14:54:12 +0100

    PYCBC-892: Finish porting v2 tests
    Fix annotations on multi ops
    
    Change-Id: If3a9a5acb6d306edddf3af57b152deb96c814b7a

---
+++ couchbase_v2/tests/cases/xattr_t.py
@@ -0,0 +1,30 @@
+from couchbase.exceptions import PathNotFoundException
+
+from couchbase_tests.base import CollectionTestCase
+import couchbase_core.subdocument as SD
+
+
+class XattrTest(CollectionTestCase):
+    def test_xattrs_basic(self):
+        cb = self.cb
+        k = self.gen_key('xattrs')
+        cb.upsert(k, {})
+
+        # Try to upsert a single xattr
+        rv = cb.mutate_in(k, (SD.upsert('my.attr', 'value',
+                                       xattr=True,
+                                       create_parents=True),))
+        self.assertTrue(rv.success)
+
+        body = cb.get(k)
+        self.assertFalse('my' in body.content)
+        self.assertFalse('my.attr' in body.content)
+
+        # Try using lookup_in
+        rv = cb.lookup_in(k, (SD.get('my.attr'),))
+        self.assertRaises(PathNotFoundException, rv.exists, 0)
+
+        # Finally, use lookup_in with 'xattrs' attribute enabled
+        rv = cb.lookup_in(k, (SD.get('my.attr', xattr=True),))
+        self.assertTrue(rv.exists(0))
+        self.assertEqual('value', rv.content_as[str](0))
