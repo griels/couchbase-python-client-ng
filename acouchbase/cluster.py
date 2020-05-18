3ff39c09c70142a9dd4b4723115c0539e8a4365c
Ellis Breen <ellis.breen@couchbase.com>
2020-05-19 00:21:06 +0100

    Docs fix
    Change-Id: I713b72565005bcefba437e243a8b8f435a6bbb41

--- acouchbase/cluster.py
+++ acouchbase/cluster.py
@@ -1,5 +1,4 @@
-from asyncio import AbstractEventLoop, Future
-
+from asyncio import AbstractEventLoop
 
 try:
     import asyncio
@@ -15,10 +14,10 @@ from couchbase_core.client import Client as CoreClient
 from couchbase.bucket import AsyncBucket as V3AsyncBucket
 from typing import *
 from couchbase.cluster import AsyncCluster as V3AsyncCluster
+import boltons.funcutils
+from couchbase_core import IterableClass
 
 T = TypeVar('T', bound=CoreClient)
-itercls_type = TypeVar('itercls_type', bound=Iterable)
-import boltons.funcutils
 
 
 class AIOClientMixin(object):
@@ -35,48 +34,23 @@ class AIOClientMixin(object):
                    method,
                    default_iterator
                    ):
-        import wrapt
-        import inspect
-        from boltons.funcutils import wraps
-        from boltons.funcutils import update_wrapper
-        # def argspec_factory(wrapped):
-        #     argspec = inspect.getfullargspec(wrapped)
-        #
-        #
-        #     argspec.kwonlyargs+
-        #     argspec.kwonlydefaults['itercls']=default_iterator.__name__
-        #     return inspect.FullArgSpec()
-        #wrapt.decorator(adapter=argspec_factory(method))
-        import functools
-        #wrapped_method=boltons.funcutils.FunctionBuilder.from_func(method)
-        #wrapped_method.add_arg('itercls', default_iterator, kwonly=True)
-        #wrapper=wrapped_method.get_func(add_source=True)
-        #@boltons.funcutils.wraps(method)#, expected=[('itercls', default_iterator)])
         def wrapper_raw(self,
                     *args,
                     **kwargs
                     ):
             return method(self, *args, itercls=kwargs.pop('itercls', default_iterator), **kwargs)
 
-        fresh=boltons.funcutils.FunctionBuilder.from_func(method)
+        fresh = boltons.funcutils.FunctionBuilder.from_func(method)
         fresh.add_arg('itercls', default_iterator, kwonly=True)
         method_with_itercls=fresh.get_func()
-        method_with_itercls.__annotations__['itercls'] = Type[itercls_type]
-        method_with_itercls.__annotations__['return'] = itercls_type
-        wrapper=update_wrapper(wrapper_raw, method_with_itercls)
-        #wrapper=update_wrapper(wrapper_raw, func=method)
-        #wrapper=wrapper_raw
-        import typing
-        type_hints=typing.get_type_hints(method)
-
-        rtype=type_hints.get('return', Any)
+        method_with_itercls.__annotations__['itercls'] = Type[IterableClass]
+        method_with_itercls.__annotations__['return'] = IterableClass
+        wrapper = boltons.funcutils.update_wrapper(wrapper_raw, method_with_itercls)
         wrapper.__annotations__=method.__annotations__
-        wrapper.__doc__=method.__doc__.replace(getattr(rtype,'__name__','NOT_APPLICABLE'), "itercls_type")
         wrapper.__doc__+="""
         :param itercls: type of the iterable class to be returned
 
-        As for the parent method {} but returns a result of type itercls instead ({}) by default """.format(method, default_iterator)
-
+        As for the parent method {} but returns a result of type itercls instead (:class:`{}` by default)""".format(method.__name__, default_iterator.__name__)
         return wrapper
 
     @staticmethod
