f102bd07d8bad363521259c6b3787dacc205640c
Ellis Breen <ellis.breen@couchbase.com>
2020-05-22 15:18:18 +0100

    Working docs
    Change-Id: I14fd531c43eee5fbc6f8968ba2ff9ce55ca935d9

--- couchbase/asynchronous/__init__.py
+++ couchbase/asynchronous/__init__.py
@@ -1,3 +1,5 @@
+import inspect
+import sys
 from typing import TypeVar, Callable, Type
 
 from couchbase.result import ViewResult
@@ -11,32 +13,89 @@ from couchbase_core.asynchronous.rowsbase import AsyncRowsBase
 
 import functools
 import boltons.funcutils
-iterable_producer = TypeVar('iterable_producer', bound=Callable)
+import typing
 import logging
+import textwrap
+import re
 
-def wrap_async_decorator(method,   # type: iterable_producer
+iterable_producer = TypeVar('iterable_producer', bound=Callable)
+
+_type_pattern = re.compile(r'.*\(\.\.\.\)\s\-\>\s*(.*?)$')
+
+
+def get_return_type(method):
+    try:
+        hints = typing.get_type_hints(method)
+    except Exception as e:
+        return "object"
+    rtype = hints.get('return', None)
+    if rtype:
+        return str(rtype)
+    type_comment = "object"
+    parse_kwargs = {}
+    if sys.version_info < (3, 8):
+        try:
+            import typed_ast.ast3 as ast
+        except ImportError:
+            return 'object'
+    else:
+        import ast
+        parse_kwargs = {'type_comments': True}
+    try:
+        obj_ast = ast.parse(textwrap.dedent(inspect.getsource(method)), **parse_kwargs)
+    except (OSError, TypeError):
+        return "object"
+
+    def _one_child(module):
+        children = module.body  # use the body to ignore type comments
+
+        if len(children) != 1:
+            logging.warning(
+                'Did not get exactly one node from AST for "%s", got %s', str(method), len(children))
+            return
+
+        return children[0]
+
+    obj_ast = _one_child(obj_ast)
+    if obj_ast is None:
+        pass
+
+    try:
+        matches = _type_pattern.search(obj_ast.type_comment).groups('object')
+        type_comment = matches[0]
+    except Exception as e:
+        pass
+    return type_comment
+
+
+def wrap_async_decorator(method,  # type: iterable_producer
                          default_iterator=None  # type: Type[IterableClass]
                          ):
     # type: (...) -> iterable_producer
 
     def wrap(func):
-        @functools.wraps(method)
-        def wrapper(self, *args, **kwargs):
-            return func(self, *args, itercls=kwargs.pop('itercls',default_iterator), **kwargs)
-        wrapper.__annotations__=method.__annotations__
-        if 'itercls' in wrapper.__annotations__ and iterable_producer:
+        if default_iterator:
+            @functools.wraps(method)
+            def wrapper(self, *args, **kwargs):
+                return func(self, *args, itercls=kwargs.pop('itercls', default_iterator), **kwargs)
+        else:
+            wrapper = functools.wraps(method)(func)
+
+        if default_iterator:
             wrapper.__annotations__['itercls'] = Type[default_iterator]
             wrapper.__annotations__['return'] = default_iterator
         else:
-            rtype = method.__annotations__.get('return', object)
+            rtype_name = get_return_type(method)
             try:
-                rtype_name=getattr(rtype,'__qualname__', str(rtype))
-                logging.error("wrapping rtype {} for {} - got {}".format(rtype, method, rtype_name))
+                logging.error("wrapping rtype for {} - got {}".format(method, rtype_name))
                 wrapper.__annotations__['return'] = 'asyncio.futures.Future[{}]'.format(rtype_name)
             except Exception as e:
                 raise
-        wrapper.__doc__ = """{} version of `{}`.\n{}""".format('asyncio', method.__module__+"."+method.__qualname__, method.__doc__)
+        wrapper.__doc__ = """{} version of `{}`.\n{}""".format('asyncio', getattr(method, '__module__',
+                                                                                  '~') + "." + method.__qualname__,
+                                                               method.__doc__)
         return wrapper
+
     return boltons.funcutils.partial(wrap)
 
 
