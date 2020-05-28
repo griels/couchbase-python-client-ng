d8a25e7149703a2f5a0093b1cb5085779a0fa67a
Ellis Breen <ellis.breen@couchbase.com>
2020-05-28 14:38:01 +0100

    PYCBC-615: Fill in async docs
    Motivation
    ----------
    
    We need much better docs and type hints for async APIs
    
    Changes
    -------
    
    Correctly wrap documentation and annotations/type hints
    to show functionality for async APIs.
    
    We use comment parsing from typed_ast for Pythons <3.7
    to get the type hints from the comments. But
    happy to convert CBCollection to use type annotations
    instead, which is legal syntax since Python 3.0 AFAIK.
    
    Results
    -------
    
    API doc/typehint surface area for acouchbase is complete.
    It turns out some operations need to be wrapped for async
    (can be done by adding to the MEMCACHED_NOMULTI property in
    CBCollection)
    
    Txcouchbase also still to complete.
    
    Change-Id: If1e910928904332347adaca6670ff1134994a66d

--- couchbase/asynchronous/__init__.py
+++ couchbase/asynchronous/__init__.py
@@ -1,15 +1,135 @@
+from abc import ABC
+
+import inspect
+import sys
+from typing import TypeVar, Callable, Type
+
 from couchbase.result import ViewResult
 from couchbase.n1ql import QueryResult
 from couchbase.analytics import AnalyticsResult
 from couchbase.search import SearchResult
+from couchbase_core import IterableClass
 from couchbase_core.asynchronous.view import AsyncViewBase
 from couchbase.asynchronous.search import AsyncSearchRequest
 from couchbase_core.asynchronous.rowsbase import AsyncRowsBase
 
+import functools
+import boltons.funcutils
+import typing
+import logging
+import textwrap
+import re
+
+iterable_producer = TypeVar('iterable_producer', bound=Callable)
+
+_type_pattern = re.compile(r'.*\(\.\.\.\)\s\-\>\s*(.*?)$')
 
 
+def get_return_type(method):
+    try:
+        hints = typing.get_type_hints(method)
+    except Exception as e:
+        return "object"
+    rtype = hints.get('return', None)
+    if rtype:
+        return str(rtype)
+    type_comment = "object"
+    parse_kwargs = {}
+    if sys.version_info < (3, 8):
+        try:
+            import typed_ast.ast3 as ast
+        except ImportError:
+            return 'object'
+    else:
+        import ast
+        parse_kwargs = {'type_comments': True}
+    try:
+        obj_ast = ast.parse(textwrap.dedent(inspect.getsource(method)), **parse_kwargs)
+    except (OSError, TypeError):
+        return "object"
 
-class AsyncViewResult(AsyncViewBase, ViewResult):
+    def _one_child(module):
+        children = module.body  # use the body to ignore type comments
+
+        if len(children) != 1:
+            logging.warning(
+                'Did not get exactly one node from AST for "%s", got %s', str(method), len(children))
+            return
+
+        return children[0]
+
+    obj_ast = _one_child(obj_ast)
+    if obj_ast is None:
+        pass
+
+    try:
+        matches = _type_pattern.search(obj_ast.type_comment).groups('object')
+        type_comment = matches[0]
+    except Exception as e:
+        pass
+    return type_comment
+
+
+def wrap_async_decorator(method,  # type: iterable_producer
+                         default_iterator=None  # type: Type[IterableClass]
+                         ):
+    # type: (...) -> iterable_producer
+
+    def wrap(func):
+        if default_iterator:
+            @functools.wraps(method)
+            def wrapper(self, *args, **kwargs):
+                return func(self, *args, itercls=kwargs.pop('itercls', default_iterator), **kwargs)
+        else:
+            @functools.wraps(method)
+            def wrapper(self, *args, **kwargs):
+                return func(self, *args, **kwargs)
+        if default_iterator:
+            wrapper.__annotations__['itercls'] = Type[default_iterator]
+            wrapper.__annotations__['return'] = default_iterator
+        else:
+            rtype_name = get_return_type(method)
+            try:
+                logging.error("wrapping rtype for {} - got {}".format(method, rtype_name))
+                wrapper.__annotations__['return'] = 'asyncio.futures.Future[{}]'.format(rtype_name)
+            except Exception as e:
+                raise
+        wrapper.__doc__ = """{} version of `{}`. {}""".format('asyncio', getattr(method, '__module__', '~') + "." + method.__qualname__, method.__doc__)
+        return wrapper
+
+    return boltons.funcutils.partial(wrap)
+
+
+def wrap_async(method,  # type: iterable_producer
+               default_iterator  # type: Type[IterableClass]
+               ):
+    # type: (...) -> iterable_producer
+
+    @functools.wraps(method)
+    def wrapper_raw(self,
+                    *args,
+                    **kwargs
+                    ):
+        return method(self, *args, itercls=kwargs.pop('itercls',default_iterator),**kwargs)
+
+    fresh = boltons.funcutils.FunctionBuilder.from_func(method)
+    try:
+        fresh.remove_arg('itercls')
+    except ValueError:
+        pass
+    fresh.add_arg('itercls', default_iterator, kwonly=True)
+    method_with_itercls = fresh.get_func()
+    method_with_itercls.__annotations__['itercls'] = Type[default_iterator]
+    method_with_itercls.__annotations__['return'] = default_iterator
+    wrapper = boltons.funcutils.update_wrapper(wrapper_raw, method_with_itercls)
+    wrapper.__annotations__ = method.__annotations__
+    wrapper.__doc__ += """
+    :param itercls: type of the iterable class to be returned, :class:`{}` by default""".format(
+        default_iterator.__name__)
+    return wrapper
+
+
+class AsyncViewResult(AsyncViewBase, ViewResult, ABC):
     def __init__(self, *args, **kwargs):
         """
         Initialize a new AsyncViewBase object. This is intended to be
@@ -24,12 +144,12 @@ class AsyncViewResult(AsyncViewBase, ViewResult):
         ViewResult.__init__(self, *args, **kwargs)
 
 
-class AsyncQueryResult(AsyncRowsBase, QueryResult):
+class AsyncQueryResult(AsyncRowsBase, QueryResult, ABC):
     def __init__(self, *args, **kwargs):
         QueryResult.__init__(self, *args, **kwargs)
 
 
-class AsyncQueryResultBase(AsyncQueryResult, QueryResult):
+class AsyncQueryResultBase(AsyncQueryResult, QueryResult, ABC):
     def __init__(self, *args, **kwargs):
         """
         Initialize a new AsyncViewBase object. This is intended to be
@@ -44,12 +164,12 @@ class AsyncQueryResultBase(AsyncQueryResult, QueryResult):
         QueryResult.__init__(self, *args, **kwargs)
 
 
-class AsyncAnalyticsResult(AsyncRowsBase, AnalyticsResult):
+class AsyncAnalyticsResult(AsyncRowsBase, AnalyticsResult, ABC):
     def __init__(self, *args, **kwargs):
         AnalyticsResult.__init__(self, *args, **kwargs)
 
 
-class AsyncAnalyticsResultBase(AsyncAnalyticsResult, AnalyticsResult):
+class AsyncAnalyticsResultBase(AsyncAnalyticsResult, AnalyticsResult, ABC):
     def __init__(self, *args, **kwargs):
         """
         Initialize a new AsyncViewBase object. This is intended to be
@@ -64,7 +184,7 @@ class AsyncAnalyticsResultBase(AsyncAnalyticsResult, AnalyticsResult):
         AnalyticsResult.__init__(self, *args, **kwargs)
 
 
-class AsyncSearchResult(AsyncSearchRequest, SearchResult):
+class AsyncSearchResult(AsyncSearchRequest, SearchResult, ABC):
     def __init__(self, *args, **kwargs):
         """
         Initialize a new AsyncViewBase object. This is intended to be
