ab30de0d42462d1284f81db5435ca9e069a9a21f
Ellis Breen <ellis.breen@couchbase.com>
2020-09-02 18:05:42 +0100

    PYCBC-1023: Fix PyPy build with cpyext
    Change-Id: I865626d0029a85a2432d6b87327f81ac6cf3bf2b

--- cmodule.py
+++ cmodule.py
@@ -24,14 +24,8 @@ from cbuild_config import couchbase_core, install_headers, CBuildCommon
 
 
 def gen_cmodule(extoptions):
-    if platform.python_implementation() != 'PyPy':
-        print("sources are {}".format(extoptions['sources']))
-        module = Extension(str(couchbase_core+'._libcouchbase'), **extoptions)
-    else:
-        warnings.warn('The C extension libary does not work on PyPy. '
-                      'You should install the couchbase_ffi module. Installation of this '
-                      'module will continue but will be unusable without couchbase_ffi')
-        module = None
+    print("sources are {}".format(extoptions['sources']))
+    module = Extension(str(couchbase_core+'._libcouchbase'), **extoptions)
     return module
 
 
